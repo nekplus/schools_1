8afa32aaed702883940308e5d164832ee25b6b0c
nekplus <mohsenekoohi@gmail.com>
2023-08-09 22:41:32 +0300

    Poject_students_School_MandM-test.py

--- Poject_Students_School_MandM.py
+++ Poject_Students_School_MandM.py
@@ -8,7 +8,9 @@ class Student:
         
         print(f" {self.name} {self.lastName} birth in {self.birthDate} and his number is {self.mobile} and gender is {self.gender} ")
 
-student_1 = Student("sina", "omrani", "2014/03/03", "00989122322222", "male")
+student_1 = Student("shina", "omrani", "2014/03/03", "00989122322222", "male")
+
+
 
 
 
